83a33be51094bf596b34061c551f0454358c44be
EmsCode82 <emsgithub@gmail.com>
2020-05-03 14:24:51 -0400

    .

--- js/inc-dec-counter.js
+++ js/inc-dec-counter.js
@@ -1,51 +1,19 @@
 let nbr = 0;
-
-const dec = () => {
-    nbr = nbr - 1;
-    if ((nbr >= -1 && nbr <= 1)) {
-        document.getElementById("nbr").style.color = "black"
-    } else if (nbr <= -2 && nbr % 2 === 0) {
-        document.getElementById("nbr").style.color = "red";
-    }
-    if
-        (nbr % 5 === 0) {
-        document.getElementById("nbr").style.fontStyle = "italic";
-    }
-    if (nbr >= -4 && nbr <= 4) {
-        document.getElementById("nbr").style.fontStyle = "normal";
-    } 
-    if (nbr % 7 === 0) {
-        document.getElementById("nbr").style.fontWeight = "bold";
-    }
-    if (nbr >= -6 && nbr <= 6) {
-        document.getElementById("nbr").style.fontWeight = "normal";
-    }
-    document.getElementById("nbr").value = nbr;
-}
 const inc = () => {
-    nbr = nbr + 1;
-    if ((nbr >= -1 && nbr <= 1)) {
-        document.getElementById("nbr").style.color = "black";
-    } else if (nbr >= 2 && nbr % 2 === 0) {
-        document.getElementById("nbr").style.color = "blue";
-    } 
-    if (nbr % 5 === 0) {
-        document.getElementById("nbr").style.fontStyle = "italic";
-    }
-    if (nbr >= -4 && nbr <= 4) {
-        document.getElementById("nbr").style.fontStyle = "normal";
-    } 
-    if (nbr % 7 === 0) {
-        document.getElementById("nbr").style.fontWeight = "bold";
-    }
-    if (nbr >= -6 && nbr <= 6) {
-        document.getElementById("nbr").style.fontWeight = "normal";
-    }
-    document.getElementById("nbr").value = nbr;
+    nbr++;
+    display(nbr);
 }
-const display = () => {
-    document.getElementById("nbr").value = nbr;
+const dec = () => {
+    nbr--;
+    display(nbr);
+}
+const display = (nbr) => {
+    let nbrCrtl = document.getElementById("nbr");
+    nbrCrtl.value = nbr;
+    nbrCrtl.style.color = (nbr % 2 == 0) ? "red" : "black";
+    nbrCrtl.style.fontStyle = (nbr % 5 == 0) ? "italic" : "normal";
+    nbrCrtl.style.fontWeight = (nbr % 7 == 0) ? "bold" : "normal";
 }
 const loaded = () => {
-    display();
+    display(nbr);
 }
